61ffbbe576082fdbd3e26235a6961e64bbbce844
Melissa Cadena <melissabcadena@Melissas-MBP.attlocal.net>
2020-10-12 17:30:56 -0500

    add errors to form

--- addCustomer.php
+++ addCustomer.php
@@ -1,34 +1,38 @@
 
 <?php 
 
+$errors = array('name'=> "", 'email'=> '', 'phone'=>'');
+$name='Full name';
+$email='Email';
+$phone='123-456-7890';
+
   // was the form submitted
   if(isset($_POST['submit'])){
 
     // check that form was filled out correctly
     if(empty($_POST['name'])) {
-      echo 'Please enter name! <br />';
+      
+      $errors['name'] = 'Please enter name! <br />';
     } else {
-      echo $_POST['name'];
-    };
+      $name = $_POST['name'];
+    }
 
     if(empty($_POST['email'])) {
-      echo 'Please enter email! <br />';
+      $errors['email'] = 'Please enter email! <br />';
     } else {
       $email = $_POST['email'];
       if(!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-        echo 'Please enter valid email! <br />';
+        $errors['email'] = 'Please enter valid email! <br />';
       }
     };
 
     if(empty($_POST['phone'])) {
-      echo 'Please enter phone number! <br />';
+      $errors['phone'] = 'Please enter phone number! <br />';
     } else {
-      $phone = filter_var($_POST['phone'], FILTER_SANITIZE_NUMBER_INT);
-      $validphone = str_replace("-", "", $phone);
-      if (strlen($validphone) < 10 || strlen($validphone) > 14) {
-        echo 'Please enter valid phone number! <br />';
-      } else {
-        echo $validphone;
+      $cleanPhone = filter_var($_POST['phone'], FILTER_SANITIZE_NUMBER_INT);
+      $phone = str_replace("-", "", $cleanPhone);
+      if (strlen($phone) < 10 || strlen($phone) > 14) {
+        $errors['phone'] = 'Please enter valid phone number! <br />';
       }
     };
   }
@@ -48,19 +52,22 @@
       <div class="form-group row">
         <label for="inputCustomer" class="col-sm-2 col-form-label">Customer Name</label>
         <div class="col-sm-10">
-          <input type="text" class="form-control" id="inputCustomer" name="name" placeholder="Full Name">
+          <input type="text" class="form-control" id="inputCustomer" name="name" value="<?php echo $name; ?>" >
+          <div class='text-danger'><?php echo $errors['name'];?></div>
         </div>
       </div>
       <div class="form-group row">
         <label for="email" class="col-sm-2 col-form-label">Email</label>
         <div class="col-sm-10">
-          <input type="text" class="form-control" id="inputEmail" name="email" placeholder="Email">
+          <input type="text" class="form-control" id="inputEmail" name="email" value="<?php echo $email; ?>">
+          <div class='text-danger'><?php echo $errors['email'];?></div>
         </div>
       </div>
       <div class="form-group row">
         <label for="inputNumber" class="col-sm-2 col-form-label">Phone Number</label>
         <div class="col-sm-10">
-          <input type="text" class="form-control" id="inputPhone" name="phone" placeholder="123.456.7890">
+          <input type="text" class="form-control" id="inputPhone" name="phone" value="<?php echo $phone; ?>">
+          <div class='text-danger'><?php echo $errors['phone'];?></div>
         </div>
       </div>
       <div class="form-group row">
