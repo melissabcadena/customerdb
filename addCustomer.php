0b6642eb89e1a2412ef3952a4053f2fbdaef9dfd
Melissa Cadena <melissabcadena@Melissas-MBP.attlocal.net>
2020-10-12 16:53:31 -0500

    add some form validation

--- addCustomer.php
+++ addCustomer.php
@@ -3,10 +3,37 @@
 
   // was the form submitted
   if(isset($_POST['submit'])){
-    echo $_POST['name'];
-    echo $_POST['email'];
-    echo $_POST['phone'];
+
+    // check that form was filled out correctly
+    if(empty($_POST['name'])) {
+      echo 'Please enter name! <br />';
+    } else {
+      echo $_POST['name'];
+    };
+
+    if(empty($_POST['email'])) {
+      echo 'Please enter email! <br />';
+    } else {
+      $email = $_POST['email'];
+      if(!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        echo 'Please enter valid email! <br />';
+      }
+    };
+
+    if(empty($_POST['phone'])) {
+      echo 'Please enter phone number! <br />';
+    } else {
+      $phone = filter_var($_POST['phone'], FILTER_SANITIZE_NUMBER_INT);
+      $validphone = str_replace("-", "", $phone);
+      if (strlen($validphone) < 10 || strlen($validphone) > 14) {
+        echo 'Please enter valid phone number! <br />';
+      } else {
+        echo $validphone;
+      }
+    };
   }
+
+
 
 ?>
 
@@ -27,13 +54,13 @@
       <div class="form-group row">
         <label for="email" class="col-sm-2 col-form-label">Email</label>
         <div class="col-sm-10">
-          <input type="email" class="form-control" id="inputEmail" name="email" placeholder="Email">
+          <input type="text" class="form-control" id="inputEmail" name="email" placeholder="Email">
         </div>
       </div>
       <div class="form-group row">
         <label for="inputNumber" class="col-sm-2 col-form-label">Phone Number</label>
         <div class="col-sm-10">
-          <input type="number" class="form-control" id="inputPhone" name="phone" placeholder="123.456.7890">
+          <input type="text" class="form-control" id="inputPhone" name="phone" placeholder="123.456.7890">
         </div>
       </div>
       <div class="form-group row">
