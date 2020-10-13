7356e7cfd60dd0569b4370dad9e766fe01c7e215
Melissa Cadena <melissabcadena@Melissas-MBP.attlocal.net>
2020-10-13 17:15:55 -0500

    add customer notes

--- customer.php
+++ customer.php
@@ -18,14 +18,39 @@ if(isset($_POST['delete'])) {
 // check get request id
 if(isset($_GET['id'])) {
     $id = mysqli_real_escape_string($conn, $_GET['id']);
+
+    // grab customer info
     $sql = "SELECT * FROM customer_info WHERE id = $id";
-    
+
     $result = mysqli_query($conn, $sql);
     $customer = mysqli_fetch_assoc($result);
 
+    // grab customer notes
+    $sqlNotes = "SELECT note FROM `customer_notes` WHERE user_id = $id";
+
+
+    $notesResult = mysqli_query($conn, $sqlNotes);
+    $allNotes = mysqli_fetch_all($notesResult);
     mysqli_free_result($result);
+    mysqli_free_result($notesResult);
+
     mysqli_close($conn);
 }
+
+// add new note form submitted
+if(isset($_POST['newNote'])){
+    $id_to_add = mysqli_real_escape_string($conn, $_POST['id_to_add']);
+    $note = mysqli_real_escape_string($conn, $_POST['note-text']);
+    $sql = "INSERT INTO customer_notes(note, user_id) VALUES('$note', '$id_to_add')";
+    // if successful
+    if(mysqli_query($conn, $sql)) {
+        header("Location: index.php");
+    } else {
+        // if not successful
+        echo 'query error: ' . mysqli_error($conn);
+    }
+}
+
 ?>
 
 <!DOCTYPE html>
@@ -50,7 +75,7 @@ if(isset($_GET['id'])) {
     </tr>
   </thead>
   <tbody>
-  <?php if($customer) : ?>
+  <?php if($customer) :?>
     <tr>
       <th scope="row"><?php echo $customer['id']?></th>
       <td><?php echo $customer['name']?></td>
@@ -66,6 +91,7 @@ if(isset($_GET['id'])) {
 
     </tr>
 
+
   <?php else: ?>
     <h4> No Customer with this ID!</h4>
   <?php endif; ?>
@@ -74,5 +100,29 @@ if(isset($_GET['id'])) {
 
   </tbody>
 </table>
+
+    <!-- Customer Notes -->
+    <h4>Customer Notes: </h4>
+    
+ <!-- loops through notes to create li for each note -->
+     <ul class="list-group">
+         <?php foreach($allNotes as $note){ 
+            foreach($note as $singleNote) { ?>
+        <li class="list-group-item"><?php echo $singleNote?></li>
+        <?php }
+    } ?>  
+    </ul> 
+
+   
+    <!-- new note form -->
+    <div>
+        <form role="form" method="POST" action="customer.php">
+            <textarea name="note-text" class="form-control" id="exampleFormControlTextarea1" rows="5"></textarea>
+            <input type="hidden" name="id_to_add" value="<?php echo $customer['id'] ?>">
+        ,    <input type="submit" value="Add note!" name="newNote" class="btn btn-primary"/>
+        </form>
+    </div>
+
+
 
 <?php include('templates/footer.php'); ?>
